d2aff3b39d1bcb594c3bafb90439e1d6d2c0fb0a
sanjeeb2017 <sanjeebiitg@gmail.com>
2023-03-25 22:34:03 +0000

    terrform set up

---
+++ Week1/code/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-east-2"
+}
+
