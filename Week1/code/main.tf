bbdf0e813f48d9e193be2071d6afb44ec07a3e79
Sanjeeb Mohapatra <sanjeebiitg@gmail.com>
2023-03-26 10:56:51 +0100

    Update main.tf

--- Week1/code/main.tf
+++ Week1/code/main.tf
@@ -9,6 +9,25 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-east-2"
+  region = "eu-west-2"
+}
+
+# Create a Key pair
+
+resource "aws_key_pair" "sanjeeb-aws-key-pair" {
+  key_name   = "sanjeeb-aws-key-pair"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+# RSA key of size 4096 bits
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create a local file
+resource "local_file" "sanjeeb-aws-key-pair" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "sanjeeb-aws-key-pair"
 }
 
