506c63fd88edfdfd094bf6e3ea98df0d9ebb72cc
Sanjeeb Mohapatra <sanjeebiitg@gmail.com>
2023-03-29 22:25:51 +0100

    Add files via upload

---
+++ Week2/code/code/sanjeeb-aws-week-2/variable.tf
@@ -0,0 +1,9 @@
+variable "project_name" {}
+variable "region" {}
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_subnet_app_az1_cidr" {}
+variable "private_subnet_app_az2_cidr" {}
+variable "private_subnet_data_az1_cidr" {}
+variable "private_subnet_data_az2_cidr" {}
